eb3259cc165028ba8730e8a69860bc15c4fe4d00
Jose Loya Marquez <81578887+loyamarjTZ@users.noreply.github.com>
2021-04-20 16:21:52 -0600

    Update Trapezoid.java

--- Trapezoid.java
+++ Trapezoid.java
@@ -13,80 +13,77 @@ public class Trapezoid {
 	static int topLength;
 	static int bottomLength;	
 	static char fillerSymbol;
-    static boolean validInput;
+    	static boolean validInput;
 	static Scanner scan = new Scanner(System.in);
 	
 	//Main function
 	public static void main(String[] args) {  
-	    obtainTopLength();
+		obtainTopLength();
 		obtainBottomLength();
-        displayTrapezoid();
+        	displayTrapezoid();
 	}        
       
 	//Gets the top length of the trapezoid
 	public static void obtainTopLength()	{
 		validInput = false;
 		do{
-            //Prompts the user to enter the top length of the trapezoid.
-            System.out.println("Please enter an ODD integer in the range 1 - 15: ");
+            		//Prompts the user to enter the top length of the trapezoid.
+            		System.out.println("Please enter an ODD integer in the range 1 - 15: ");
 			topLength = scan.nextInt();
-            System.out.println("\nYou entered: " + topLength);
+            		System.out.println("\nYou entered: " + topLength);
             
-            //Verifies if the topLength value is valid.
-			if( (topLength>=1 && topLength<=15) && (topLength%2 != 0) )
-			{
-                System.out.println("Your input of " + topLength + " is accepted.\n");
+            		//Verifies if the topLength value is valid.
+			if( (topLength>=1 && topLength<=15) && (topLength%2 != 0) ){
+                		System.out.println("Your input of " + topLength + " is accepted.\n");
 				validInput = true;  
 			} else {
-                System.out.println("Your input of " + topLength + " is invalid.\n");
-            }
+                		System.out.println("Your input of " + topLength + " is invalid.\n");
+            		}
 		}while(validInput == false);
 	}
 	
     //Gets the bottom length of the trapezoid
-	public static void obtainBottomLength()
-	{
+	public static void obtainBottomLength(){
 		validInput = false;
-        do{
-            //Prompts the user to enter the bottom length of the trapezoid.
-            System.out.println("Please enter an ODD integer in the range 3 - 17 AND is greater than your previously accepted input: ");
-            bottomLength = scan.nextInt();
-            System.out.println("\nYou entered: " + bottomLength);
+        	do{
+            		//Prompts the user to enter the bottom length of the trapezoid.
+            		System.out.println("Please enter an ODD integer in the range 3 - 17 AND is greater than your previously accepted input: ");
+            		bottomLength = scan.nextInt();
+            		System.out.println("\nYou entered: " + bottomLength);
 
-            //Verifies if the bottomLength value is valid.
-            if( (bottomLength>=3 && bottomLength<=17) && (bottomLength%2 != 0) && (bottomLength>topLength)) {
-                System.out.println("Your input of " + bottomLength + " is accepted.\n");
-                validInput = true;
-			}else{
-                System.out.println("Your input of " + bottomLength + " is invalid.\n");
-            }
-        }while(validInput == false);
+            		//Verifies if the bottomLength value is valid.
+            		if( (bottomLength>=3 && bottomLength<=17) && (bottomLength%2 != 0) && (bottomLength>topLength)) {
+                		System.out.println("Your input of " + bottomLength + " is accepted.\n");
+                		validInput = true;
+			} else {
+                		System.out.println("Your input of " + bottomLength + " is invalid.\n");
+            		}
+        	}while(validInput == false);
 	}
 	
     //Displays the trapezoid to the user
 	public static void displayTrapezoid() {
 		
-        //Local variables
+        	//Local variables
 		int numberOfChars = topLength;
 		int numberOfSpaces = (bottomLength-topLength)/2;
 
-        //Create a filled trapezoid with any character of choice
+        	//Create a filled trapezoid with any character of choice
 		System.out.println("Please enter any character: ");
 		String userInput = scan.next();
 		fillerSymbol = userInput.charAt(0);
 		
-        //Displays the trapezoid
+        	//Displays the trapezoid
 		while(numberOfSpaces>=0) {
-
-            for (int i = numberOfSpaces; i > 0; i--)
-             	System.out.print(" ");
+            		for (int i = numberOfSpaces; i > 0; i--)
+             			System.out.print(" ");
             
-            for (int j = numberOfChars; j > 0; j--)
-             	System.out.print(fillerSymbol);
+            		for (int j = numberOfChars; j > 0; j--)
+             			System.out.print(fillerSymbol);
             
-            System.out.println();
-            numberOfSpaces--;
-            numberOfChars+=2;
-        } 
-    }
+            		System.out.println();
+            		numberOfSpaces--;
+            		numberOfChars+=2;
+        	} 
+    	}
 }
